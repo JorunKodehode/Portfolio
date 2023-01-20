ca3dadfc94add09a9f868fb8e297f2b95b9ca51b
Jorun <jorun.kodehode@gmail.com>
2023-01-20 10:59:10 +0100

    tr

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,10 +4,12 @@ export function Home() {
   return (
     <div>
       <h2 className="onClickAvatar">KLIKK på MEG</h2>
-      <td>
-        <img src={logo} alt="Avatar karakter av Jorun" />
-      </td>
-      <td></td>
+      <tr>
+        <td>
+          <img src={logo} alt="Avatar karakter av Jorun" />
+        </td>
+        <td></td>
+      </tr>
     </div>
   );
 }
